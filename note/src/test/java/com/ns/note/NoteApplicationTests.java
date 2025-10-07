dc06691b70ac9321330b446e14b28a103ced93e9
minwoonggi <d34sda334@gmail.com>
2025-10-07 19:50:15 +0900

    Chore: Mockito note Integration test option #16

--- note/src/test/java/com/ns/note/NoteApplicationTests.java
+++ note/src/test/java/com/ns/note/NoteApplicationTests.java
@@ -1,10 +1,14 @@
 package com.ns.note;
 
+import com.ns.note.note.repository.NoteRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@SpringBootTest
+@SpringBootTest()
 class NoteApplicationTests {
+    @MockitoBean
+	private NoteRepository noteRepository;
 
 	@Test
 	void contextLoads() {
