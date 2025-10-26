2958b7435382f3b1e6853bd650274602ef24e07c
minwoonggi <d34sda334@gmail.com>
2025-10-26 14:59:36 +0900

    Chore: Test code editing

--- note/src/test/java/com/ns/note/note/entity/NoteEntityTest.java
+++ note/src/test/java/com/ns/note/note/entity/NoteEntityTest.java
@@ -20,7 +20,7 @@ public class NoteEntityTest{
                 .build();
 
         // when
-        note.update("1", "new title", "new desc", "new contents");
+        note.update("1", "new title", "new desc", "new contents", null);
 
         // then
         assertThat(note.getWorkspaceId()).isEqualTo("1");
