016996debc632a3c3df2cffd26fe45ebed1dd2be
downfa11 <downfa11@naver.com>
2025-10-28 23:01:08 +0900

    Feat: add corsWebFilter
    Signed-off-by: downfa11 <downfa11@naver.com>

--- gateway/src/main/java/com/ns/gateway/config/CorsConfig.java
+++ gateway/src/main/java/com/ns/gateway/config/CorsConfig.java
@@ -4,7 +4,7 @@ package com.ns.gateway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
@@ -13,28 +13,21 @@ import java.util.Arrays;
 public class CorsConfig {
 
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
+    public CorsWebFilter corsWebFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOriginPatterns(Arrays.asList(
                 "http://localhost:5173",
                 "http://127.0.0.1:5173",
-                "https://pkm-frontend-zeta.vercel.app/"
-        ));
+                "https://pkm-frontend-zeta.vercel.app"));
 
-        config.setAllowedMethods(Arrays.asList(
-                "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"
-        ));
-
-        config.setAllowedHeaders(Arrays.asList(
-                "Authorization", "Content-Type", "X-Requested-With"
-        ));
-
+        config.setAllowedMethods(Arrays.asList("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
+        config.setAllowedHeaders(Arrays.asList("Authorization","Content-Type","X-Requested-With"));
         config.setAllowCredentials(true);
         config.setMaxAge(3600L);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
 
-        return source;
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
     }
 }
