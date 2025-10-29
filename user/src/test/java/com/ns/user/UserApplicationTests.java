1263379ee002d897a02409768443e1a5df6bf2b4
downfa11 <downfa11@naver.com>
2025-10-29 14:01:54 +0900

    Fix: add dummy fields to test
    Signed-off-by: downfa11 <downfa11@naver.com>

--- user/src/test/java/com/ns/user/UserApplicationTests.java
+++ user/src/test/java/com/ns/user/UserApplicationTests.java
@@ -2,8 +2,21 @@ package com.ns.user;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "jwt.secret=AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", 
+
+    "google.client.id=dummy-google-id",
+    "google.client.secret=dummy-google-secret",
+    "google.client.redirect-uri=http://test.local/auth/google/callback",
+    
+    "yorkie.jwt.secret=BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
+    "yorkie.webhook.secret=dummy-webhook-secret",
+    
+    "frontend.redirect.url=http://test.local/main"
+})
 class UserApplicationTests {
 
 	@Test
